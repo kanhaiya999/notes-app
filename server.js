312ce311ca0e0a469eb5f8caaae4109dbca5439c
kanhaiya lal <kanhaiyalalkesar@gmail.com>
2022-08-01 17:24:01 +0530

    kk

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/NOTES'));
+app.use(express.static('./dist/notes'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', { root: 'dist/NOTES' }),
+    res.sendFile('index.html', { root: 'dist/notes' }),
 );
 
 // Start the app by listening on the default Heroku port
